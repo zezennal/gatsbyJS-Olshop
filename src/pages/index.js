d12760d3e1e242e025fd1a9aa6dbbfbd0fb2607f
nall <zezennal@gmail.com>
2019-01-27 15:21:57 +0700

    add button

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,16 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { HomeHeader, Banner } from '../utils'
+import { HomeHeader, Banner, BannerButton } from '../utils'
 import img from '../images/home-bg.jpg'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <HomeHeader img={img}>
-      <Banner title="Hire Me" subtitle="Front End Developer - HTML - CSS - JAVASCRIPT"/>
+      <Banner title="Hire Me" subtitle="Front End Developer - HTML - CSS - JAVASCRIPT">
+        <BannerButton style={{margin: '2rem auto'}}>menu</BannerButton>
+      </Banner>
     </HomeHeader>
   </Layout>
 )
